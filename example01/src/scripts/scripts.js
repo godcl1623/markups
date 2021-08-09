52563d7bb0461c992d0af5ebfe19e1477bbb407a
godcl1623 <delphi5281@gmail.com>
2021-08-09 23:47:20 +0900

    markups - develop - 검색버튼 자바스크립트 적용중

---
+++ example01/src/scripts/scripts.js
@@ -0,0 +1,10 @@
+/* eslint-disable strict */
+
+'use strict';
+
+// 검색 버튼 이벤트 - 검색창 표시
+// search button event - to display search term input
+const searchBtn = document.querySelector('.search-form button');
+searchBtn.addEventListener('click', e => {
+  e.preventDefault();
+});
